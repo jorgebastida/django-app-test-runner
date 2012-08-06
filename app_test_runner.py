87ab5195fd625c13bfcaeba1603481220a6a4b42
Jorge Bastida <me@jorgebastida.com>
2012-08-06 14:39:51 +0200

    Fix TEMPLATE_LOADERS

--- app_test_runner.py
+++ app_test_runner.py
@@ -12,15 +12,15 @@ def main():
     """
     The entry point for the script. This script is fairly basic. Here is a
     quick example of how to use it::
-    
+
         app_test_runner.py [path-to-app]
-    
+
     You must have Django on the PYTHONPATH prior to running this script. This
     script basically will bootstrap a Django environment for you.
-    
+
     By default this script with use SQLite and an in-memory database. If you
     are using Python 2.5 it will just work out of the box for you.
-    
+
     TODO: show more options here.
     """
     parser = OptionParser()
@@ -29,9 +29,9 @@ def main():
     parser.add_option("--DATABASE_USER", dest="DATABASE_USER", default="")
     parser.add_option("--DATABASE_PASSWORD", dest="DATABASE_PASSWORD", default="")
     parser.add_option("--SITE_ID", dest="SITE_ID", type="int", default=1)
-    
+
     options, args = parser.parse_args()
-    
+
     # check for app in args
     try:
         app_path = args[0]
@@ -56,8 +56,9 @@ def main():
         "SITE_ID": options.SITE_ID,
         "ROOT_URLCONF": "",
         "TEMPLATE_LOADERS": (
-            "django.template.loaders.filesystem.load_template_source",
-            "django.template.loaders.app_directories.load_template_source",
+            "django.template.loaders.filesystem.Loader",
+            "django.template.loaders.app_directories.Loader",
+            "django.template.loaders.eggs.Loader",
         ),
         "TEMPLATE_DIRS": (
             os.path.join(os.path.dirname(__file__), "templates"),
